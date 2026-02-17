80cc88c361376b4d91c9754763912ccbf79957fd
Jatin481-dev <s65257361@gmail.com>
2026-02-17 15:20:29 +0530

    fix: avoid remote Google Fonts during build; use system font stack

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { GoogleTagManager } from "@next/third-parties/google";
-import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
@@ -7,7 +6,8 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-const inter = Inter({ subsets: ["latin"] });
+// Using system font stack to avoid remote Google Fonts fetch during build
+const inter = { className: "font-sans" };
 
 export const metadata = {
   title: "Portfolio of Jatin Kumar - Software Developer",
